b0469966ee820ff8959d448fc38408221aa7c9d1
Danny Le <dung.lequang@nashtechglobal.com>
2018-03-07 12:04:50 +1100

    init commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+const express = require ('express');
+
+const publicPath = path.join(__dirname, "/../public");
+const port = process.env.PORT || 3000;
+var app = express();
+var server =http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  socket.on('disconnect', ()=>{
+    console.log('user disconnected');
+  })
+});
+server.listen(port, ()=> {
+  console.log(`server is up on ${port}`);
+});
